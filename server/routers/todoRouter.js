3f8806a28edad0595f966de12bb64a6f059466da
SisiYin <t3chyi00@students.oamk.fi>
2024-11-01 03:05:34 +0200

    Part_5

--- server/routers/todoRouter.js
+++ server/routers/todoRouter.js
@@ -14,25 +14,25 @@ router.get('/',(req,res,next) => {
   })
 })
 
-router.post('/create', (req,res) => {
+router.post('/create', (req,res,next) => {
   pool.query('insert into task (description) values ($1) returning *',
     [req.body.description],
     (error,result) => {
       if (error) {
-        return res.status(500).json({error: error.message})
+        return next(error)
       }
-      return res.status(200).json({id: result.rows[0].id})
+      return res.status(200).json({id: emptyOrRows(result)[0].id})
     }
   )
 })
 
-router.delete("/delete/:id",async(req,res)=> {
+router.delete("/delete/:id",async(req,res,next)=> {
   const id = parseInt(req.params.id)
   
   pool.query('delete from task where id = $1',[id],
   (error,result) => {
     if (error) {
-      return res.status(500).json({error: error.massage})
+      return next(error)
     } 
     return res.status(200).json({id:id})
     
