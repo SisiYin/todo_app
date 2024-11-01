b777755f491b2ee17fd06ef8296dd0a4d0e5067f
SisiYin <t3chyi00@students.oamk.fi>
2024-11-01 02:57:11 +0200

    Part_5

--- server/index.js
+++ server/index.js
@@ -1,62 +1,64 @@
 import express from 'express'
 import cors from 'cors'
-import pkg from 'pg'
+import todoRouter from './routers/todoRouter.js'
 
-const port = 3001 
-const {Pool} = pkg
+const port = process.env.PORT 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use('/',todoRouter)
 
-app.get('/',(req,res) => {
-  const pool = openDb()
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({ error: err.message });
+});
 
-  pool.query('select * from task',(error, result) => {
-  if (error) {
-    return res.status(500).json({error: error.message})
-  }
-  return res.status(200).json(result.rows)
-})
-})
 
-app.post('/create', (req,res) => {
-  const pool = openDb()
+// app.get('/',(req,res) => {
+//   pool.query('select * from task',(error, result) => {
+//   if (error) {
+//     return res.status(500).json({error: error.message})
+//   }
+//   return res.status(200).json(result.rows)
+// })
+// })
 
-  pool.query('insert into task (description) values ($1) returning *',
-    [req.body.description],
-    (error,result) => {
-      if (error) {
-        return res.status(500).json({error: error.message})
-      }
-      return res.status(200).json({id: result.rows[0].id})
-    }
-  )
-})
+// app.post('/create', (req,res) => {
+//   pool.query('insert into task (description) values ($1) returning *',
+//     [req.body.description],
+//     (error,result) => {
+//       if (error) {
+//         return res.status(500).json({error: error.message})
+//       }
+//       return res.status(200).json({id: result.rows[0].id})
+//     }
+//   )
+// })
 
-app.delete("/delete/:id",async(req,res)=> {
-  const pool = openDb()
-  const id = parseInt(req.params.id)
+// app.delete("/delete/:id",async(req,res)=> {
+//   const id = parseInt(req.params.id)
   
-  pool.query('delete from task where id = $1',[id],
-  (error,result) => {
-    if (error) {
-      return res.status(500).json({error: error.massage})
-    } 
-    return res.status(200).json({id:id})
+//   pool.query('delete from task where id = $1',[id],
+//   (error,result) => {
+//     if (error) {
+//       return res.status(500).json({error: error.massage})
+//     } 
+//     return res.status(200).json({id:id})
     
-  })
-})
+//   })
+// })
 
-const openDb = () => {
-  const pool = new Pool ({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'todo',
-    password: '251423',
-    port: 5432
-  })
-  return pool
-}
+// const openDb = () => {
+//   const pool = new Pool ({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: environment === 'development'? process.env.DB_NAME : process.env.TEST_DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT
+//   })
+//   return pool
+// }
+
 app.listen(port)
